56dda653ef2cc3de852db0ea93c96bd849f42667
y <y>
2024-05-16 14:42:38 -0500

    shit

--- System/Modules/bash/bash.nix
+++ System/Modules/bash/bash.nix
@@ -3,7 +3,7 @@
   imports = [];
   programs.bash.shellAliases = {
    update = "cd ~/vhal-nixos/ && nix flake update  && sudo nixos-rebuild switch --flake ~/nixos/#default && cd";
-   switch = "sudo nixos-rebuild switch --flake ~/vhal-nixos/#default";
+   switch = "cd ~/vhal-nixos/ && git add . && git commit -am "switcheroo" && sudo nixos-rebuild switch --flake ~/vhal-nixos/#default";
    cd = "z";
    vim = "lvim";
    nvim = "lvim";
