6c42369278294858f4f5bc48ad47df5fe0c6b337
y <y>
2024-05-16 14:54:08 -0500

    user-switch

--- System/Modules/bash/bash.nix
+++ System/Modules/bash/bash.nix
@@ -4,6 +4,7 @@
   programs.bash.shellAliases = {
    update = "cd ~/vhal-nixos/ && nix flake update  && sudo nixos-rebuild switch --flake ~/nixos/#default && cd";
    switch = "cd ~/vhal-nixos/ && git add . && git commit -am user-switch && sudo nixos-rebuild switch --flake ~/vhal-nixos/#default";
+   backup = "cd ~/vhal-nixos/ && git add . && git commit -am backup && git push";
    cd = "z";
    vim = "lvim";
    nvim = "lvim";
